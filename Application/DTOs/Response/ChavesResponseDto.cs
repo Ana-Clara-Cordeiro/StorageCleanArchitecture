e56dc0564347b8f3f31927274c13b190756588b0
girlando-junior <girlandojunior209@gmail.com>
2025-11-10 23:37:36 -0300

    criando interface de chaves

--- Application/DTOs/Response/ChavesResponseDto.cs
+++ Application/DTOs/Response/ChavesResponseDto.cs
@@ -8,5 +8,13 @@ namespace Storage.Application.DTOs.Response
 {
     internal class ChavesResponseDto
     {
+        public string Id { get; set; }
+
+        public string Nome { get; set; }
+
+        public string Descricao { get; set; }
+
+        public int Quantidade { get; set; }
+
     }
 }
