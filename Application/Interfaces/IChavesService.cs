a0b03792119c71bca26d99794e3a64c22f549dd6
girlando-junior <girlandojunior209@gmail.com>
2025-11-10 23:58:24 -0300

    criando service

--- Application/Interfaces/IChavesService.cs
+++ Application/Interfaces/IChavesService.cs
@@ -3,10 +3,12 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using Storage.Application.DTOs.Response;
 
 namespace Storage.Application.Interfaces
 {
     internal interface IChavesService
     {
+        Task<List<ChavesResponseDto>> ObterTodos();
     }
 }
