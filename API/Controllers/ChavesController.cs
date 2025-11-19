5a3227ffebddb0dd08b24bd16f1c9db1a6fc5016
girlando-junior <girlandojunior209@gmail.com>
2025-11-18 23:41:49 -0300

    criando rota de cadastrar chave

--- API/Controllers/ChavesController.cs
+++ API/Controllers/ChavesController.cs
@@ -2,6 +2,7 @@
 using Microsoft.AspNetCore.Mvc;
 using Storage.Application.Services;
 using Storage.Application.Services.Interfaces;
+using Storage.Application.DTOs.Request;
 using Storage.Domain.Entities;
 using Swashbuckle.AspNetCore.Annotations;
 
@@ -36,5 +37,10 @@ namespace Storage.API.Controllers
             return Ok(chave);
         }
 
+        [HttpPost]
+        public async Task<ActionResult> Cadastrar([FromBody] CadastrarChavesRequestDto request)
+        {
+            return Ok(await this.service.Cadastrar(request));
+        }
     }
 }
---
+++ Application/DTOs/Request/CadastrarChavesRequestDto.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Storage.Application.DTOs.Request
+{
+    public class CadastrarChavesRequestDto
+    {
+        public string Nome { get; set; }
+
+        public string Descricao { get; set; }
+
+        public int Quantidade { get; set; }
+    }
+}
